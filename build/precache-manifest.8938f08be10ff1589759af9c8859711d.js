3b9653f69a249c68b54012d73151126fa3741ba1
Pei Xuan Ong <opx@Peis-MacBook-Pro.local>
2020-02-25 21:24:59 +0800

    change config

---
+++ build/precache-manifest.8938f08be10ff1589759af9c8859711d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d7f9c1b1b6fe46d092346d58a3539b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef4df715b42fe86b8c6d",
+    "url": "/static/js/2.0e804a2e.chunk.js"
+  },
+  {
+    "revision": "9f52ed86d7f3375f7646",
+    "url": "/static/js/main.0273165c.chunk.js"
+  },
+  {
+    "revision": "99c8aa31ff1b89981831",
+    "url": "/static/js/runtime-main.8536cb82.js"
+  }
+]);
